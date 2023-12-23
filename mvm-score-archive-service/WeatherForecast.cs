b61dec6f857c6654b5d3250e531380893d4fd882
Jakob Herrle GH <92975853+jakoherr@users.noreply.github.com>
2023-12-23 11:45:41 +0100

    Add service

---
+++ mvm-score-archive-service/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace Mvm.Score.Archive.Service;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
