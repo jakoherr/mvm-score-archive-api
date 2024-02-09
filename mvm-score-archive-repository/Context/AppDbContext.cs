97e27623a2c28db82b9295ad8078061fed7a1d06
Jakob Herrle GH <92975853+jakoherr@users.noreply.github.com>
2024-02-09 10:43:46 +0100

    Added endpoint

--- mvm-score-archive-repository/Context/AppDbContext.cs
+++ mvm-score-archive-repository/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Mvm.Score.Archive.Repository.DbEntities;
 
 namespace Mvm.Score.Archive.Repository.Context;
 
@@ -7,5 +8,17 @@ public sealed class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {        
+    }
+
+    public DbSet<Composer> Composers => this.Set<Composer>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Composer>(e =>
+        {
+            e.HasKey(e => e.Id);
+        });
     }
 }
---
+++ mvm-score-archive-service/ServiceAutomapperConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Mvm.Score.Archive.Repository.DbEntities;
+using Mvm.Score.Archive.Service.Composers;
+
+namespace Mvm.Score.Archive.Service;
+
+public class ServiceAutomapperConfiguration : Profile
+{
+    public ServiceAutomapperConfiguration()
+    {
+        this.CreateMap<ComposerDto, Composer>();
+    }
+}
