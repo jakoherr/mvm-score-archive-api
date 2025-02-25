bc81232e8fd122d87c3d9c8ea8e0bf565c8d066e
Jakob Herrle GH <jakobherrle@hotmail.de>
2025-02-25 16:09:14 +0100

    add order and wrote migration

--- mvm-score-archive-repository/DbEntities/DbPart.cs
+++ mvm-score-archive-repository/DbEntities/DbPart.cs
@@ -7,13 +7,15 @@ public class DbPart
 {
     public int Id { get; set; }
 
-    required public string Instrument { get; set; }
+    public required string Instrument { get; set; }
 
     public int? Part { get; set; }
 
     public Tunings Tuning { get; set; }
 
     public Clef Clef { get; set; }
+
+    public int SortOrder { get; set; }
 
     public string FileName
     {
